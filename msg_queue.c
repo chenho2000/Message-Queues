986dcfe7806fb9cafb736808897546b873cc8ef1
gongmin9 <m.gong@mail.utoronto.ca>
2021-07-12 14:01:52 -0400

    init

---
+++ msg_queue.c
@@ -0,0 +1,274 @@
+/*
+ * This code is provided solely for the personal and private use of students
+ * taking the CSC369H course at the University of Toronto. Copying for purposes
+ * other than this use is expressly prohibited. All forms of distribution of
+ * this code, including but not limited to public repositories on GitHub,
+ * GitLab, Bitbucket, or any other online platform, whether as given or with
+ * any changes, are expressly prohibited.
+ *
+ * Authors: Alexey Khrabrov, Andrew Pelegris, Karen Reid
+ *
+ * All of the files in this directory and all subdirectories are:
+ * Copyright (c) 2019, 2020 Karen Reid
+ */
+
+/**
+ * CSC369 Assignment 2 - Message queue implementation.
+ *
+ * You may not use the pthread library directly. Instead you must use the
+ * functions and types available in sync.h.
+ */
+
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include "errors.h"
+#include "list.h"
+#include "msg_queue.h"
+#include "ring_buffer.h"
+
+
+// Message queue implementation backend
+typedef struct mq_backend {
+	// Ring buffer for storing the messages
+	ring_buffer buffer;
+
+	// Reference count
+	size_t refs;
+
+	// Number of handles open for reads
+	size_t readers;
+	// Number of handles open for writes
+	size_t writers;
+
+	// Set to true when all the reader handles have been closed. Starts false
+	// when they haven't been opened yet.
+	bool no_readers;
+	// Set to true when all the writer handles have been closed. Starts false
+	// when they haven't been opened yet.
+	bool no_writers;
+
+	//TODO: add necessary synchronization primitives, as well as data structures
+	//      needed to implement the msg_queue_poll() functionality
+
+} mq_backend;
+
+
+static int mq_init(mq_backend *mq, size_t capacity)
+{
+	if (ring_buffer_init(&mq->buffer, capacity) < 0) {
+		return -1;
+	}
+
+	mq->refs = 0;
+
+	mq->readers = 0;
+	mq->writers = 0;
+
+	mq->no_readers = false;
+	mq->no_writers = false;
+
+	//TODO: initialize remaining fields (synchronization primitives, etc.)
+
+	return 0;
+}
+
+static void mq_destroy(mq_backend *mq)
+{
+	assert(mq->refs == 0);
+	assert(mq->readers == 0);
+	assert(mq->writers == 0);
+
+	ring_buffer_destroy(&mq->buffer);
+
+	//TODO: cleanup remaining fields (synchronization primitives, etc.)
+}
+
+
+#define ALL_FLAGS (MSG_QUEUE_READER | MSG_QUEUE_WRITER | MSG_QUEUE_NONBLOCK)
+
+// Message queue handle is a combination of the pointer to the queue backend and
+// the handle flags. The pointer is always aligned on 8 bytes - its 3 least
+// significant bits are always 0. This allows us to store the flags within the
+// same word-sized value as the pointer by ORing the pointer with the flag bits.
+
+// Get queue backend pointer from the queue handle
+static mq_backend *get_backend(msg_queue_t queue)
+{
+	mq_backend *mq = (mq_backend*)(queue & ~ALL_FLAGS);
+	assert(mq);
+	return mq;
+}
+
+// Get handle flags from the queue handle
+static int get_flags(msg_queue_t queue)
+{
+	return (int)(queue & ALL_FLAGS);
+}
+
+// Create a queue handle for given backend pointer and handle flags
+static msg_queue_t make_handle(mq_backend *mq, int flags)
+{
+	assert(((uintptr_t)mq & ALL_FLAGS) == 0);
+	assert((flags & ~ALL_FLAGS) == 0);
+	return (uintptr_t)mq | flags;
+}
+
+
+static msg_queue_t mq_open(mq_backend *mq, int flags)
+{
+	++mq->refs;
+
+	if (flags & MSG_QUEUE_READER) {
+		++mq->readers;
+		mq->no_readers = false;
+	}
+	if (flags & MSG_QUEUE_WRITER) {
+		++mq->writers;
+		mq->no_writers = false;
+	}
+
+	return make_handle(mq, flags);
+}
+
+// Returns true if this was the last handle
+static bool mq_close(mq_backend *mq, int flags)
+{
+	assert(mq->refs != 0);
+	assert(mq->refs >= mq->readers);
+	assert(mq->refs >= mq->writers);
+
+	if ((flags & MSG_QUEUE_READER) && (--mq->readers == 0)) {
+		mq->no_readers = true;
+	}
+	if ((flags & MSG_QUEUE_WRITER) && (--mq->writers == 0)) {
+		mq->no_writers = true;
+	}
+
+	if (--mq->refs == 0) {
+		assert(mq->readers == 0);
+		assert(mq->writers == 0);
+		return true;
+	}
+	return false;
+}
+
+
+msg_queue_t msg_queue_create(size_t capacity, int flags)
+{
+	if (flags & ~ALL_FLAGS) {
+		errno = EINVAL;
+		report_error("msg_queue_create");
+		return MSG_QUEUE_NULL;
+	}
+
+	// Refuse to create a message queue without capacity for
+	// at least one message (length + 1 byte of message data).
+	if ( capacity < (sizeof(size_t) + 1) ) {
+		errno = EINVAL;
+		report_error("msg_queue_create");
+		return MSG_QUEUE_NULL;
+	}
+	
+	mq_backend *mq = (mq_backend*)malloc(sizeof(mq_backend));
+	if (!mq) {
+		report_error("malloc");
+		return MSG_QUEUE_NULL;
+	}
+	// Result of malloc() is always aligned on 8 bytes, allowing us to use the
+	// 3 least significant bits of the handle to store the 3 bits of flags
+	assert(((uintptr_t)mq & ALL_FLAGS) == 0);
+
+	if (mq_init(mq, capacity) < 0) {
+		// Preserve errno value that can be changed by free()
+		int e = errno;
+		free(mq);
+		errno = e;
+		return MSG_QUEUE_NULL;
+	}
+
+	return mq_open(mq, flags);
+}
+
+msg_queue_t msg_queue_open(msg_queue_t queue, int flags)
+{
+	if (!queue) {
+		errno = EBADF;
+		report_error("msg_queue_open");
+		return MSG_QUEUE_NULL;
+	}
+
+	if (flags & ~ALL_FLAGS) {
+		errno = EINVAL;
+		report_error("msg_queue_open");
+		return MSG_QUEUE_NULL;
+	}
+
+	mq_backend *mq = get_backend(queue);
+
+	//TODO: add necessary synchronization
+
+	msg_queue_t new_handle = mq_open(mq, flags);
+
+	return new_handle;
+}
+
+int msg_queue_close(msg_queue_t *queue)
+{
+	if (!queue || !*queue) {
+		errno = EBADF;
+		report_error("msg_queue_close");
+		return -1;
+	}
+
+	mq_backend *mq = get_backend(*queue);
+
+	//TODO: add necessary synchronization
+
+	if (mq_close(mq, get_flags(*queue))) {
+		// Closed last handle; destroy the queue
+		mq_destroy(mq);
+		free(mq);
+		*queue = MSG_QUEUE_NULL;
+		return 0;
+	}
+
+	//TODO: if this is the last reader (or writer) handle, notify all the writer
+	//      (or reader) threads currently blocked in msg_queue_write() (or
+	//      msg_queue_read()) and msg_queue_poll() calls for this queue.
+
+	*queue = MSG_QUEUE_NULL;
+	return 0;
+}
+
+
+ssize_t msg_queue_read(msg_queue_t queue, void *buffer, size_t length)
+{
+	//TODO
+	(void)queue;
+	(void)buffer;
+	(void)length;
+	errno = ENOSYS;
+	return -1;
+}
+
+int msg_queue_write(msg_queue_t queue, const void *buffer, size_t length)
+{
+	//TODO
+	(void)queue;
+	(void)buffer;
+	(void)length;
+	errno = ENOSYS;
+	return -1;
+}
+
+
+int msg_queue_poll(msg_queue_pollfd *fds, size_t nfds)
+{
+	//TODO
+	(void)fds;
+	(void)nfds;
+	errno = ENOSYS;
+	return -1;
+}
